1662cdc7085b39d52576164b6e701a94f058cc8a
tanawatgravitechdev <tanawatgravitechdev@gmail.com>
2024-05-15 15:55:37 +0700

    add comment 🌟

--- main.py
+++ main.py
@@ -7,7 +7,10 @@ sum_text = ""
 dataJson = []
 tempDesignator = []
 number = 0
+
+#fetch data from file to list
 for i in range(sh.nrows-1):
+    #if frist index than add item only
     if len(dataJson) == 0:
         dataJson.append({
             "part_type":sh.cell_value(rowx=i+1, colx=0),
@@ -17,6 +20,7 @@ for i in range(sh.nrows-1):
         })
         number += 1
     else:
+        #if dataJson use to have data in colum 0 and colum 2. concat data in designator by last index
         if dataJson[number-1]['part_type'] == sh.cell_value(rowx=i+1, colx=0) and dataJson[number-1]['foot_print'] == sh.cell_value(rowx=i+1, colx=2):
             dataJson[number-1]['designator'] = dataJson[number-1]['designator']+','+sh.cell_value(rowx=i+1, colx=1)
         else:
@@ -27,8 +31,12 @@ for i in range(sh.nrows-1):
                 "description":sh.cell_value(rowx=i+1, colx=3)
             })
             number += 1
+
+#create file
 workbook = xlwt.Workbook()
 sheet = workbook.add_sheet('output')
+
+#loop data insert
 number = 0
 for i in dataJson:
     sheet.write(number, 0, i['part_type'])
@@ -37,4 +45,5 @@ for i in dataJson:
     sheet.write(number, 3, i['description'])
     number+=1
 
+#save file
 workbook.save('output.xls')
